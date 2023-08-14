125d0b07f486b3c1672225de787df0918bd98000
sami <sami@ajz123>
2023-08-14 14:57:29 +0300

    first commit

---
+++ gg/src/App.jsx
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className="title">react test</h1>
+     <h2 className="name">sami</h2>
+    </>
+  );
+}
+
+export default App;
